c003cafae4f389d6bd7cec229673201b299d8c23
imjjun <90594374+imjjun@users.noreply.github.com>
2023-01-10 01:11:18 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from movie import movie
+import pandas as pd
+
+url_list=movie.getting_movieurl('https://pedia.watcha.com/ko-KR/users/DgwxAeQYNxrMj/contents/movies/ratings')
+overview=movie.movie_overview(url_list)
+overview.to_csv('/Users/jungjunlim/desktop/Python/Watchapedia_movie3_DB.csv')
