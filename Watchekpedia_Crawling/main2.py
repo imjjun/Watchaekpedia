8f5fd447a5132ce52ea1f37a3c0f0a768c468030
imjjun <90594374+imjjun@users.noreply.github.com>
2023-01-10 21:29:58 +0900

    Update main2.py

--- Watchekpedia_Crawling/main2.py
+++ Watchekpedia_Crawling/main2.py
@@ -4,5 +4,5 @@ from book import book
 
 url_list=book.getting_movieurl('https://pedia.watcha.com/ko-KR/users/DgwxAeQYNxrMj/contents/books/ratings')
 overview=book.book_overview(url_list)
-overview.to_csv('/Users/jungjunlim/desktop/Python/Watchapedia_book_DB.csv')
+overview.to_csv('insert your local address')
 
